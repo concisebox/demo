c4dbeae7a8cda3d63c5646d6c384947a13d51362
Lucky Lim <lucky.a.lim@gmail.com>
2016-07-24 17:06:10 +0100

    Initialize Project

---
+++ techOne/src/test/java/TestServer.java
@@ -0,0 +1,22 @@
+
+public class TestServer {
+
+    private String apple;
+
+    public TestServer(String apple) {
+        this.apple = apple;
+    }
+
+    public String getApple() {
+        return apple;
+    }
+
+    public void setApple(String apple) {
+        this.apple = apple;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
